715f6bb6718b4767ea4536d9406e8db2a99fcc1c
ACBob <acbobcat@protonmail.com>
2021-07-07 16:48:24 +0100

    Basic functionality

--- main.py
+++ main.py
@@ -3,24 +3,32 @@
 
 import web
 from web.contrib.template import render_jinja
+from scss import compiler
 
 urls = (
+	'/(.*)\.css', 'stylesheet', # Preprocesses the stylesheet with sass
 	'/(.*)', 'pronoun'
 )
 app = web.application(urls, globals())
 
 builtin_pronouns = [
 	["he", "him", "his"],
-	["she", "her", "hers"],
-	["they", "them", "theirs"],
-	["it", "that", "its"] # Neo
+	["she", "her", "her"],
+	["they", "them", "their"],
+
+	# Commonly used neopronouns, from a list that claimed 'he' was a neopronoun
+	["e", "em", "eir"],
+	["per", "per", "pers"],
+	["ve", "ver", "vis"],
+	["ze", "hir", "hir"],
+	["zie", "hir", "hir"]
 ]
 
 render_page = render_jinja('pages', encoding='utf-8')
 
 class pronoun:
-	def GET(self, name):
-		pronouns = name.split('/')
+	def GET(self, args):
+		pronouns = args.split('/')
 
 		print(pronouns)
 
@@ -32,22 +40,32 @@ class pronoun:
 			a = pronouns[0]
 			for n in builtin_pronouns:
 				if a == n[0]:
-					return ', '.join(n)
+					pronouns = [pronoun.capitalize() for pronoun in n]
+					return render_page.pronouns(pronouns=pronouns)
 			
 			return "Pronoun not found, qualify more!"
 		elif len(pronouns) == 2:
 			# Try a built-in again
 			a = pronouns[0]
 			for n in builtin_pronouns:
-				if a == n[0]:
-					return ', '.join(n)
+				# Test the next one so that he/she works (OOPS)
+				if a == n[0] and pronouns[1] == n[1]:
+					pronouns = [pronoun.capitalize() for pronoun in n]
+					return render_page.pronouns(pronouns=pronouns)
 			
-			# Two is enough to construct pronouns
-			return "TODO"
+			# Two is enough to construct SOME pronouns
+			pronouns = [n.capitalize() for n in pronouns]
+			return render_page.pronouns(pronouns=pronouns)
 		elif len(pronouns) == 3:
-			return ', '.join(pronouns)
+			pronouns = [n.capitalize() for n in pronouns]
+			return render_page.pronouns(pronouns=pronouns)
 		
 		return "Too many qualifiers."
+
+class stylesheet: # TODO: is it inefficient to serve it every time? Maybe compile the scss when ran and serve that!
+	def GET(self, args):
+		return compiler.compile_file(args+".scss")
+
 
 if __name__ == "__main__":
 	app.run()
